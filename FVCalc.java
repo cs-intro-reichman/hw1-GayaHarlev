f1c97c1bb55f586a456a866e9e6821f1570b8113
GayaHarlev <gaia.harlev@gmail.com>
2024-11-08 14:30:04 +0200

    commit solution9

--- FVCalc.java
+++ FVCalc.java
@@ -8,7 +8,7 @@ public class FVCalc {
         // חישוב ערך עתידי (future value)
         double futureValue = currentValue * Math.pow(1 + rate / 100.0, n);
 
-        // הדפסת התוצאה בפורמט הנדרש עם סימן דולר ועם %25 עבור סימן אחוז
-        System.out.printf("After %d years, $%d saved at %.1f%% will yield $%d%n", n, currentValue, rate, (int) futureValue);
+        // הדפסת התוצאה בפורמט הנדרש
+        System.out.printf("After %d years, a $%d saved at %.1f%% will yield $%d%n", n, currentValue, rate, (int) futureValue);
     }
 }
